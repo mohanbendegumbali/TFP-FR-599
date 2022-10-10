dfb27523721f4f5cba70a4c0487efd5b31ba13de
mohanbendegumbali <mohan0311@hotmail.com>
2022-10-10 10:59:54 +0530

    [Mohan]-Employee-Salary

---
+++ empsalary.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -x
+
+isparttime=1;
+isfulltime=2;
+randomcheck=$((RANDOM%3));
+
+if [ $isfulltime -eq $randomcheck ];
+then
+    emphrs=8;
+elif [ $isparttime -eq $randomcheck ];
+then
+    emphrs=4;
+else
+    emphrs=0;
+fi
+
+salary=$(( emphrs*20 ));
