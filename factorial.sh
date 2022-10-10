22e45c8c511c57d277912a0549dd106fb3a71a7f
mohanbendegumbali <mohan0311@hotmail.com>
2022-10-10 10:25:36 +0530

    [Mohan]-Factorial

---
+++ factorial.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -x
+
+echo "Enter a number:"
+read number1
+result=1
+
+for (( number=1; number<=$number1; number++ ))
+do
+
+    result=$(( $result*$number ))
+done
+
+echo " factorial of a  number is:$result "
