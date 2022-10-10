33e506c40803fb1f9b029322e805d79998a76ec2
mohanbendegumbali <mohan0311@hotmail.com>
2022-10-10 11:19:37 +0530

    [Mohan]-Palindrome-using-Function

---
+++ palindrome-function.sh
@@ -0,0 +1,25 @@
+#!/bin/bash -x
+
+sum=0
+reminder=0
+
+function palindrome()
+{
+    number=$(($2))
+    temp=$number
+    while  [ $number -gt 0 ]
+    do
+       reminder=$(($number%10))
+       sum=$((($sum*10)+$reminder))
+       number=$(($number/10))
+    done
+    if [  $temp  -eq $sum  ]
+     then
+       echo "Palindrome"
+     else
+      echo "Noo"
+    fi
+
+}
+palindrome 12
+
